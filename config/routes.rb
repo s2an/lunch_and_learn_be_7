f9fc84fbc08f83cb384dcce383d4cedf3b9fa2cd
Nash <141100397+s2an@users.noreply.github.com>
2024-03-05 11:57:01 -0500

    Add fixture for real and route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :recipes, only: [:index]
       resources :learning_resources, only: [:show]
+      resources :tourist_sites, only: [:index]
     end
   end
 end
