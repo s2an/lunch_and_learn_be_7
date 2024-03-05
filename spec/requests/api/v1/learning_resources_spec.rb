bf2b6366ebe43b7b30cd658653e0397627cf96e0
Nash <141100397+s2an@users.noreply.github.com>
2024-03-04 23:21:48 -0500

    test: Create intial test for youtube connection

--- spec/requests/api/v1/learning_resources_spec.rb
+++ spec/requests/api/v1/learning_resources_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::LearningResources', type: :request do
-  it "returns the correct response and data from pexels"  do
+  xit "PEXELS: returns the correct response and data"  do
     q = "Ethiopia"
 
-    pexels_response = File.read("spec/fixtures/pexels_ethiopia.json")
+    pexels_response = File.read("spec/fixtures/pexels_#{q}.json")
     stub_request(:get, "https://api.pexels.com/v1/search?query=#{q}").
       to_return(status: 200, body: pexels_response, headers: {Authorization: Rails.application.credentials.pexels[:authorization]})
     
@@ -16,5 +16,22 @@ RSpec.describe 'Api::V1::LearningResources', type: :request do
 
     expect(parsed_pexels).to be_a(Hash)
     expect(parsed_pexels[:data]).to be_an(Array)
+  end
+
+  it "YOUTUBE: returns the correct response and data"  do
+    q = "Ethiopia"
+
+    youtube_response = File.read("spec/fixtures/youtube_#{q.downcase}.json")
+    stub_request(:get, "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCluQ5yInbeAkkeCndNnUhpw&q=#{q}&key=#{Rails.application.credentials.youtube[:key]}").
+      to_return(status: 200, body: youtube_response, headers: {})
+    
+    get "/api/v1/learning_resources?country=#{q}"
+
+    expect(response).to have_http_status(200)
+    
+    parsed_youtube = JSON.parse(youtube_response.body, symbolize_names: true)
+
+    expect(parsed_youtube).to be_a(Hash)
+    expect(parsed_youtube[:data]).to be_an(Array)
   end
 end
