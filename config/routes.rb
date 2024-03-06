4fd466eb8a460ebd12583a6afe289404f52c8359
Nash <141100397+s2an@users.noreply.github.com>
2024-03-06 03:33:03 -0500

    Create some initial tests

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index]
-      resources :learning_resources, only:[:index]
+      resources :learning_resources, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:password_digest)}
+  end
+  
+  it "generates an API key" do
+    user = User.create!(name: "Odell", email: "goodboy@ruffruff.com", password: "password123")
+    
+    expect(user).to have(:api_key)
+    expect(user.api_key.status_code).to eq(201)
+    #flash message <--feature
+  end
+
+  it "forces a secure password" do
+    user = User.create!(name: "Odell", email: "goodboy@ruffruff.com", password: "password123")
+    
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq("password123")
+  end
+
+  #feature
+  xit "has unique email" do
+    user_1 = User.create!(name: "Odell", email: "goodboy@ruffruff.com", password: "treats4lyf")
+    user_2 = User.create!(name: "Adele", email: "goodboy@ruffruff.com", password: "sing4lyf")
+    
+    expect(user_1).to be_an_instance_of(User)
+    expect(user_2).to_not be_an_instance_of(User)
+    # flash message!
+  end
+
+  #feature
+  xit "forces matching passwords" do
+    user_1 = User.create!(name: "Odell", email: "goodboy@ruffruff.com", password: "treats4lyf", password_confirmation: "n0treats4u")
+    
+    expect(user_1).to be_an_instance_of(User)
+    expect(user).to_not have_attribute(:password)
+    # flash message!
+  end
 end
