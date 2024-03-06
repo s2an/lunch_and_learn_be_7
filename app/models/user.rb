eb4a7a8e1e489caf7d51f736530478ab540639a7
Nash <141100397+s2an@users.noreply.github.com>
2024-03-06 03:07:29 -0500

    Generate ApiKey and make association to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password_digest
 
+  has_many :api_keys, as: :bearer 
+
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_075450) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_080339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.integer "bearer_id", null: false
+    t.string "bearer_type", null: false
+    t.string "token", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["bearer_id", "bearer_type"], name: "index_api_keys_on_bearer_id_and_bearer_type"
+    t.index ["token"], name: "index_api_keys_on_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
